86460a09c6a3b3ce4e277375d57a10343babb88b
Salman laptop <t123y2@hotmail.com>
2024-02-06 12:13:29 +0300

    some refactoring

---
+++ src/components/Ui/Filter.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import arrowIcon from "../../assets/icons/arrow.png";
+
+export default function Filter({ filterOpen, setFilterOpen, selectedFilter, setSelectedFilter }) {
+     const handleSelectedFilter = (region) => {
+       setSelectedFilter(region);
+       setFilterOpen(false);
+     };
+  return (
+    <div className="relative w-1/2 lg:w-1/3 xl:w-1/4">
+      <div
+        className="flex justify-between items-center cursor-pointer bg-white shadow-md rounded-lg px-4 py-4"
+        onClick={() => setFilterOpen(!filterOpen)}
+      >
+        <p className="text-base font-semibold">{selectedFilter}</p>
+        <img
+          src={arrowIcon}
+          className={`transform ${filterOpen ? "rotate-180" : ""} size-4`}
+          alt="arrow"
+        />
+      </div>
+
+      {filterOpen && (
+        <div className="absolute w-full bg-white shadow-md rounded-lg px-4 py-4 mt-2">
+          <div className="flex flex-col">
+            <p
+              className="cursor-pointer text-base font-semibold p-2 rounded hover:bg-gray-300"
+              onClick={() => handleSelectedFilter("Africa")}
+            >
+              Africa
+            </p>
+            <p
+              className="cursor-pointer text-base font-semibold p-2 rounded hover:bg-gray-300"
+              onClick={() => handleSelectedFilter("America")}
+            >
+              America
+            </p>
+            <p
+              className="cursor-pointer text-base font-semibold p-2 rounded hover:bg-gray-300"
+              onClick={() => handleSelectedFilter("Asia")}
+            >
+              Asia
+            </p>
+            <p
+              className="cursor-pointer text-base font-semibold p-2 rounded hover:bg-gray-300"
+              onClick={() => handleSelectedFilter("Europe")}
+            >
+              Europe
+            </p>
+            <p
+              className="cursor-pointer text-base font-semibold p-2 rounded hover:bg-gray-300"
+              onClick={() => handleSelectedFilter("Oceania")}
+            >
+              Oceania
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
