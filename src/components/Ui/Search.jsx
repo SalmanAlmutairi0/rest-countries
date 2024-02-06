2d5a3f5406d7ce71c52e0318c9574b6c617b416e
Salman laptop <t123y2@hotmail.com>
2024-02-06 11:27:40 +0300

    add: search functionality

--- src/components/Ui/Search.jsx
+++ src/components/Ui/Search.jsx
@@ -1,27 +1,97 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import arrowIcon from "../../assets/icons/arrow.png";
-
+import img from "../../assets/nf.svg";
+import { Link } from "react-router-dom";
 export default function Search({ selectedFilter, setSelectedFilter }) {
   const [filterOpen, setFilterOpen] = useState(false);
-
+  const [searchText, setSearchText] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [closeSearch, setCloseSearch] = useState(false);
   const handleSelectedFilter = (region) => {
     setSelectedFilter(region);
     setFilterOpen(false);
   };
 
+  const handleSearch = (e) => {
+    if (e.target.value === "") {
+      setSearchResult([]);
+      return;
+    }
+    setSearchText(() => e.target.value);
+  };
+
+  useEffect(() => {
+    const getCountryBySearch = async () => {
+      try {
+        const res = await fetch(
+          `https://restcountries.com/v3.1/name/${searchText}`
+        );
+        const data = await res.json();
+        if (Array.isArray(data)) {
+          // Check if data is an array
+          setSearchResult(data);
+          setCloseSearch(true);
+        } else {
+          setSearchResult([]);
+          setCloseSearch(false);
+        }
+      } catch (error) {
+        console.error("Error fetching country data:", error);
+      }
+    };
+    getCountryBySearch();
+  }, [searchText]);
+
   return (
     <div className="py-8">
-      <div className="container flex flex-col  w-[95%] mx-auto justify-center gap-16 lg:flex-row lg:justify-between lg:items-center">
-        <form className="bg-white shadow-md rounded-lg flex items-center gap-4 px-4 py-4  md:w-1/2 lg:w-1/3 xl:w-1/4">
-          <span role="img" aria-label="search-icon">
-            🔍
-          </span>
-          <input
-            type="text"
-            placeholder="Search for a country..."
-            className="outline-none w-full"
-          />
-        </form>
+      <div className="container flex flex-col  w-[95%] mx-auto  justify-center  gap-16 lg:flex-row lg:justify-between lg:items-center">
+        {/* search */}
+        <div className="flex flex-col relative md:w-1/2 lg:w-1/3 xl:w-1/4 ">
+          <form
+            className="bg-white shadow-md rounded-lg flex items-center gap-4 px-4 py-4  w-full"
+            onChange={(e) => handleSearch(e)}
+          >
+            <span role="img" aria-label="search-icon">
+              🔍
+            </span>
+            <input
+              type="text"
+              placeholder="Search for a country..."
+              className="outline-none w-full"
+            />
+          </form>
+
+          {/* result */}
+          {closeSearch && (
+            
+            <div className="absolute top-16 z-20 w-full bg-white shadow-md rounded-lg px-4 py-4 mt-2">
+            <div className="flex flex-col gap-5">
+              {/* each row */}
+              {searchResult.map((country, index) => (
+                index < 5 &&
+                  <Link to={`/details/${country.ccn3}`} >
+                <div key={index} className="flex border p-1 cursor-pointer gap-4 hover:bg-slate-100">
+                  <img
+                    src={country?.flags?.svg || img}
+                    className="w-24"
+                    alt={country?.name?.common}
+                    />
+                  <p className="text-base font-semibold">
+                    {country?.name?.common}
+                  </p>
+                </div>
+                  </Link>
+              ))}
+
+              {searchResult.length === 0 && (
+                <p className="text-base font-semibold">No results found</p>
+              )}
+
+             
+            </div>
+          </div>
+        )}
+        </div>
 
         <div className="relative w-1/2 lg:w-1/3 xl:w-1/4">
           <div
